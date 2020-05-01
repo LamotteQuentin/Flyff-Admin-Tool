9a92b2bcafb735408f884491c2636d8d3ffe110d
Quentin Lamotte <quentin.lamotte@actimage.com>
2020-05-01 15:24:26 +0200

    Modification du menu.
    Ajout de l'icône de l'application.
    Traduction des liens.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,12 +1,20 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark" class="shadow">
-    <b-navbar-brand to="/">FAT</b-navbar-brand>
+    <b-navbar-brand to="/" class="d-flex align-items-center">
+      <b-img src="/icon.png" class="mr-2" height="32" />
+      FAT
+    </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item to="/odbc">ODBC</b-nav-item>
+        <b-nav-item to="/system">
+          {{ $t('components.menu.links.system') }}
+        </b-nav-item>
+        <b-nav-item to="/odbc">
+          {{ $t('components.menu.links.odbc') }}
+        </b-nav-item>
       </b-navbar-nav>
 
       <b-navbar-nav class="ml-auto">
